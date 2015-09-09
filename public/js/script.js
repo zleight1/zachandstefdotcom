99d3cdf5f0f9d9631160dff4615375098486b627
Zachary Leighton <zleight1@gmail.com>
2015-09-09 19:52:44 -0400

    Removed some sections, fixed countdown.

--- public/js/script.js
+++ public/js/script.js
@@ -335,7 +335,7 @@ $(document).ready(function() {
 	// 07. COUNTDOWN
 	//===================================================================================
 	var theday = new Date();
-	theday = new Date(2015, 7, 12);
+	theday = new Date(2016, 4, 10);
 	$('#countdown').countdown({until: theday, format: 'WDHMS'});
 	$('#countdown').countdown($.countdown.regionalOptions['custom-label']); 
 		 
